d0116814eded005cd2f036ea04e66ac8e5d25b0c
Léana 江 <leana.jiang@icloud.com>
2023-04-26 21:47:06 +0200

    feat: basic image output

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,58 @@
-// use stft::{WindowType, STFT};
+use plotters::{
+    prelude::{BitMapBackend, ChartBuilder, Circle, IntoDrawingArea},
+    style::{Color, GREEN, WHITE},
+};
+use stft::{WindowType, STFT};
 
 fn main() {
-    // let mut reader = claxon::FlacReader::open("audio/field_of_innocence.flac").unwrap();
-    // let samples: Vec<f64> = reader
-    //     .samples()
-    //     .into_iter()
-    //     .map(|sample| sample.unwrap() as f64)
-    //     .collect();
-    //
-    // let window_type = WindowType::Hanning;
-    // let window_size = 1024;
-    // let step_size = 512;
-    //
-    // let mut stft: STFT<f64> = STFT::new(window_type, window_size, step_size);
-    //
-    // for sample in (samples[..]).chunks(3000) {
-    //     stft.append_samples(sample);
-    //
-    //     while stft.contains_enough_to_compute() {
-    //         let mut buffer: Vec<_> = std::iter::repeat(0.).take(stft.output_size()).collect();
-    //         stft.compute_column(&mut buffer);
-    //         println!("Column {:?}", buffer);
-    //         stft.move_to_next_column();
-    //     }
-    // }
+    let mut reader = claxon::FlacReader::open("audio/field_of_innocence.flac").unwrap();
+    let samples: Vec<f64> = reader
+        .samples()
+        .into_iter()
+        .map(|sample| sample.unwrap() as f64)
+        .collect();
+
+    let window_type = WindowType::Hanning;
+    let window_size = 2048;
+    let step_size = 1024;
+
+    let mut stft: STFT<f64> = STFT::new(window_type, window_size, step_size);
+    let mut columns: Vec<Vec<f64>> = Vec::new();
+
+    for sample in (samples[..]).chunks(4100) {
+        stft.append_samples(sample);
+
+        while stft.contains_enough_to_compute() {
+            let mut buffer: Vec<_> = std::iter::repeat(0.).take(stft.output_size()).collect();
+            stft.compute_column(&mut buffer);
+            columns.push(buffer);
+            // println!("{:?}", buffer);
+            stft.move_to_next_column();
+        }
+    }
+    println!("{}", columns.len());
+
+    let canvas = BitMapBackend::new("image.png", (1920, 1080)).into_drawing_area();
+
+    canvas.fill(&WHITE).unwrap();
+
+    let mut plot_ctx = ChartBuilder::on(&canvas)
+        .build_cartesian_2d(0_f64..27007_f64, 0_f64..2048_f64)
+        .unwrap();
+
+    for (x, column) in columns.iter().enumerate() {
+        plot_ctx
+            .draw_series(
+                column
+                    .iter()
+                    .filter(|x| **x > 2_f64)
+                    .enumerate()
+                    .map(|(y, amplitude)| Circle::new((x as f64, y as f64), 2, GREEN.stroke_width(*amplitude as u32))),
+            )
+            .unwrap();
+    }
+
+    canvas.present().unwrap();
+
+    ()
 }
