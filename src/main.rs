e07d2ecf0a5c99a439942c784d12c22295ef6909
Léana 江 <leana.jiang@icloud.com>
2023-04-26 17:37:38 +0200

    add: core functionality seems to work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+// use stft::{WindowType, STFT};
+
 fn main() {
-    println!("Hello, world!");
+    // let mut reader = claxon::FlacReader::open("audio/field_of_innocence.flac").unwrap();
+    // let samples: Vec<f64> = reader
+    //     .samples()
+    //     .into_iter()
+    //     .map(|sample| sample.unwrap() as f64)
+    //     .collect();
+    //
+    // let window_type = WindowType::Hanning;
+    // let window_size = 1024;
+    // let step_size = 512;
+    //
+    // let mut stft: STFT<f64> = STFT::new(window_type, window_size, step_size);
+    //
+    // for sample in (samples[..]).chunks(3000) {
+    //     stft.append_samples(sample);
+    //
+    //     while stft.contains_enough_to_compute() {
+    //         let mut buffer: Vec<_> = std::iter::repeat(0.).take(stft.output_size()).collect();
+    //         stft.compute_column(&mut buffer);
+    //         println!("Column {:?}", buffer);
+    //         stft.move_to_next_column();
+    //     }
+    // }
 }
