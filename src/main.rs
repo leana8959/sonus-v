7478f3d0ce81a8b6de61da595582bee04e951c1f
Léana 江 <leana.jiang@icloud.com>
2023-04-27 00:06:21 +0200

    kinda working...

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use plotters::{
     prelude::{BitMapBackend, ChartBuilder, Circle, IntoDrawingArea},
-    style::{Color, GREEN, WHITE},
+    series::SurfaceSeries,
+    style::{Color, HSLColor, GREEN, WHITE},
 };
 use stft::{WindowType, STFT};
+
+const WINDOW_SIZE: usize = 8192;
+const STEP_SIZE: usize = 4096;
 
 fn main() {
     let mut reader = claxon::FlacReader::open("audio/field_of_innocence.flac").unwrap();
@@ -13,17 +17,15 @@ fn main() {
         .collect();
 
     let window_type = WindowType::Hanning;
-    let window_size = 2048;
-    let step_size = 1024;
 
-    let mut stft: STFT<f64> = STFT::new(window_type, window_size, step_size);
-    let mut columns: Vec<Vec<f64>> = Vec::new();
+    let mut stft: STFT<f64> = STFT::new(window_type, WINDOW_SIZE, STEP_SIZE);
+    let mut columns: Vec<[f64; WINDOW_SIZE / 2]> = Vec::new();
 
-    for sample in (samples[..]).chunks(4100) {
+    for sample in (samples[..]).chunks(5000) {
         stft.append_samples(sample);
 
         while stft.contains_enough_to_compute() {
-            let mut buffer: Vec<_> = std::iter::repeat(0.).take(stft.output_size()).collect();
+            let mut buffer = [0.0; WINDOW_SIZE / 2];
             stft.compute_column(&mut buffer);
             columns.push(buffer);
             // println!("{:?}", buffer);
@@ -32,27 +34,32 @@ fn main() {
     }
     println!("{}", columns.len());
 
-    let canvas = BitMapBackend::new("image.png", (1920, 1080)).into_drawing_area();
+    let canvas = BitMapBackend::new("image.png", (600, 400)).into_drawing_area();
 
     canvas.fill(&WHITE).unwrap();
 
+    // for pitch in 0..10 {
     let mut plot_ctx = ChartBuilder::on(&canvas)
-        .build_cartesian_2d(0_f64..27007_f64, 0_f64..2048_f64)
+        .build_cartesian_3d(
+            0_f64..columns.len() as f64,
+            0_f64..WINDOW_SIZE as f64 * 0.75,
+            0_f64..100_f64,
+        )
         .unwrap();
 
-    for (x, column) in columns.iter().enumerate() {
-        plot_ctx
-            .draw_series(
-                column
-                    .iter()
-                    .filter(|x| **x > 2_f64)
-                    .enumerate()
-                    .map(|(y, amplitude)| Circle::new((x as f64, y as f64), 2, GREEN.stroke_width(*amplitude as u32))),
+    plot_ctx
+        .draw_series(
+            SurfaceSeries::xoz(
+                (0..columns.len()).map(|x| x as f64),
+                (0..columns[0].len()).map(|y| y as f64),
+                |x, y| columns[x as usize][y as usize] as f64 * 500.0,
             )
-            .unwrap();
-    }
+            .style_func(&|&v| (&HSLColor(100.0 + v, 1.0, 0.7)).mix(0.8).filled()),
+        )
+        .unwrap();
+
+    println!("Done");
 
     canvas.present().unwrap();
-
-    ()
+    // }
 }
