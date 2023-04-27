e717e34a92ab97b31f8132beb4009d78896677ae
Léana 江 <leana.jiang@icloud.com>
2023-04-27 11:44:55 +0200

    ref: code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
-use plotters::{prelude::*, series::SurfaceSeries};
+use raylib::prelude::*;
 use stft::{WindowType, STFT};
 
-const WINDOW_SIZE: usize = 8192;
-const STEP_SIZE: usize = 4096;
+const WINDOW_SIZE: usize = 2048;
+const STEP_SIZE: usize = 1024;
+const WIDTH: i32 = 1400;
+const HEIGHT: i32 = 900;
 
 // const IMAGE_WIDTH: u32 = 1920;
 // const IMAGE_HEIGHT: u32 = 1080;
 const IMAGE_WIDTH: u32 = 600;
 const IMAGE_HEIGHT: u32 = 400;
 
-fn main() {
-    let mut reader = claxon::FlacReader::open("audio/field_of_innocence.flac").unwrap();
-    // let mut reader = claxon::FlacReader::open("audio/blues.flac").unwrap();
+fn read() -> Result<Vec<f64>, Box<dyn std::error::Error>> {
+    let mut reader = claxon::FlacReader::open("audio/field_of_innocence.flac")?;
     // let mut reader = claxon::FlacReader::open("audio/sine_10mhz.flac").unwrap();
-    let samples: Vec<f64> = reader
+    Ok(reader
         .samples()
         .into_iter()
         .map(|sample| sample.unwrap() as f64)
-        .collect();
+        .collect())
+}
 
+fn create_columns(samples: Vec<f64>) -> Vec<[f64; WINDOW_SIZE / 2]> {
     let window_type = WindowType::Hanning;
 
     let mut stft: STFT<f64> = STFT::new(window_type, WINDOW_SIZE, STEP_SIZE);
@@ -31,47 +34,55 @@ fn main() {
             let mut buffer = [0.0; WINDOW_SIZE / 2];
             stft.compute_column(&mut buffer);
             columns.push(buffer);
-            // println!("{:?}", buffer);
             stft.move_to_next_column();
         }
     }
-    println!("{}", columns.len());
 
-    let canvas = BitMapBackend::new("image.png", (IMAGE_WIDTH, IMAGE_HEIGHT)).into_drawing_area();
+    columns
+}
 
-    canvas.fill(&WHITE).unwrap();
+fn main() {
+    let samples: Vec<f64> = read().unwrap();
+    let columns = create_columns(samples);
 
+    let (mut rl, thread) = raylib::init().size(WIDTH, HEIGHT).title("foo bar").build();
+    rl.set_target_fps(1);
     let column_count = columns.len();
     let column_width = columns[0].len();
 
-    let mut plot_ctx = ChartBuilder::on(&canvas)
-        .build_cartesian_3d(
-            0_f64..column_count as f64,
-            0_f64..100_f64,
-            0_f64..column_width as f64,
-        )
-        .unwrap();
+    let mut filter = 1.0;
+    while !rl.window_should_close() {
+        let mut draw_handle = rl.begin_drawing(&thread);
+        draw_handle.clear_background(Color::BLACK);
 
-    plot_ctx.with_projection(|mut p| {
-        p.pitch = 0.1;
-        // p.yaw = 0.1;
-        p.into_matrix()
-    });
+        let pixel_size = Vector2::new(
+            WIDTH as f32 / column_count as f32,
+            HEIGHT as f32 / column_width as f32,
+        );
 
-    plot_ctx.configure_axes().draw().unwrap();
+        println!("filter: {}", filter);
+        if filter < 5.0 {
+            filter += 0.5;
+        } else {
+            filter -= 0.5;
+        }
 
-    plot_ctx
-        .draw_series(
-            SurfaceSeries::xoz(
-                (0..column_count).map(|x| x as f64),                  // x axis
-                (0..column_width).map(|z| z as f64),                  // z axis
-                |x, z| columns[x as usize][z as usize] as f64 * 10.0, // calculate y axis given (x, z)
-            )
-            .style_func(&|&y| HSLColor(0.07, y / 70.0, 0.7).mix(0.1).filled()),
-        )
-        .unwrap();
-
-    println!("Done");
-
-    canvas.present().unwrap();
+        for i in 0..column_count {
+            for j in 0..column_width {
+                let rect = Rectangle::new(
+                    i as f32 * pixel_size.x,
+                    j as f32 * pixel_size.y,
+                    pixel_size.x,
+                    pixel_size.y,
+                );
+                let amplitude = columns[i][j];
+                let color = if amplitude < filter {
+                    Color::BLACK
+                } else {
+                    Color::BEIGE
+                };
+                draw_handle.draw_rectangle_rec(rect, color);
+            }
+        }
+    }
 }
